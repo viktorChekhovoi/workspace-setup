653e933d79a84e42bfe1baaf563867d348505a8a
viktorchekhovoi <viktor.chekhovoi@cat.com>
2022-02-07 11:24:03 -0600

    Fixed tmux and simplified vim setup

--- tmux.sh
+++ tmux.sh
@@ -1,5 +1,7 @@
 printf "\n\nSetting up awesome tmux\n\n"
 
-git clone https://github.com/odedlaz/tmux-onedark-theme 
-cp ./.tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
+git clone https://github.com/odedlaz/tmux-onedark-theme ~/tmux-onedark-theme
+printf "Cloned the repo\n"
+sudo cp .tmux.conf ~/.tmux.conf
+printf "Copied the config file\n"
+sudo tmux source-file ~/.tmux.conf
--- vim.sh
+++ vim.sh
@@ -7,7 +7,7 @@ sudo cp happy_hacking.vim /ect/vim/colors
 printf "\n\nInstalling awesome vim\n\n"
 sudo rm -rf ~/.vim_runtime
 git clone --depth=1 https://github.com/viktorChekhovoi/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+sh ~/.vim_runtime/install_basic_vimrc.sh
 
 rm -rf ~/vimrc
 
