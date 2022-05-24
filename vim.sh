feb4cc52dd0fc3fed390ac04a488efea9d0fef49
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-05-24 23:44:44 +0000

    fixed vim installation

--- vim.sh
+++ vim.sh
@@ -6,7 +6,7 @@ sudo mkdir /etc/vim/colors
 sudo cp configs/happy_hacking.vim /etc/vim/colors
 
 printf "\n\nInstalling awesome vim\n\n"
-cp .configs/vimrc /home/$USER/.vimrc
+cp configs/.vimrc /home/$USER/.vimrc
 # sudo rm -rf ~/.vim_runtime
 # git clone --depth=1 https://github.com/viktorChekhovoi/vimrc.git ~/.vim_runtime
 # sh ~/.vim_runtime/install_basic_vimrc.sh
