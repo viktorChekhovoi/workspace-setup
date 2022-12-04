e46fbef6c69c96e0ff5beaad6465f4363262c190
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2022-12-04 10:42:09 -0600

    Now working for bash and zsh

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,4 @@
 printf "\n\nSetting up awesome tmux\n\n"
-# sudo rm -rf /home/$USER/tmux-onedark-theme /home/$USER/.tmux.conf
-# git clone https://github.com/odedlaz/tmux-onedark-theme /home/$USER/tmux-onedark-theme
 touch /home/$USER/.tmux.conf
 cp configs/.tmux.conf /home/$USER/.tmux.conf
 printf "Copied the config file\n"
