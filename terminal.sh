6d938105c22face7584758c2e3f863cf70800ae9
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2022-12-04 10:40:55 -0600

    Updated tmux conf to automatically detect shell

--- terminal.sh
+++ terminal.sh
@@ -1,14 +1,14 @@
-printf "Setting up Terminator...\n"
-mkdir -p /home/$USER/.config/terminator/
-cp configs/terminator_config /home/$USER/.config/terminator/config
 sudo chsh -s $(which zsh)
 printf "Switched the shell to zsh, please reboot the system after the program finishes!\n"
 
 
 printf "Installing Oh My ZSH (awesome zsh)\n"
 cp configs/.zshrc /home/$USER/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+printf "Installed Oh My ZSH\n"
 
 git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git  ~/.solarized
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
